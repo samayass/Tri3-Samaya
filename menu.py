ef778ca32ad0fe12bead0b0a011b891e213ee201
samayass <samaya2024@gmail.com>
2022-03-26 12:55:43 -0700

    sub menu changes

--- menu.py
+++ menu.py
@@ -1,56 +1,108 @@
-from week0.swap import swap1_helper
-from week0.matrix import test_matrices
-from week1.infoDb import tester
-from factorial import factorialTester
-from week1.fibonacci import fibonacci_tester
+# menuy.py - function style menu
+# Imports typically listed at top
+# each import enables us to use logic that has been abstracted to other files and folders
+from week0 import animation, matrix, swap
+from week1 import fibonacci, infoDb, factorial
+
+# Two styles are supported to execute abstracted logic
+# 1. file names will be run by exec(open("filename.py").read())
+# 2. function references will be executed directly file.function()
+main_menu = [
+]
+
+# Works similarly to main_menu
+
+sub_menu_data = [
+    ["keypad", matrix.test_matrices],
+    ["infodb", infoDb.tester]
+]
+
+sub_menu_math = [
+    ["swap", swap.swap1_helper],
+    ["fibonacci", fibonacci.fibonacci_tester],
+    ["factorial", factorial.factorialTester],
+
+]
+
+sub_menu_adventure = [
+    ["tree",],
+    ["pattern", animation.ship]
+]
+
+# Menu banner is typically defined by menu owner
+border = "=" * 25
+banner = f"\n{border}\nPlease Select An Option\n{border}"
 
 
-
-menulist = {
-    1: 'Swap',
-    2: 'Tree',
-    3: 'Matrix',
-    4: 'InfoDb',
-    5: 'Factorial',
-    6: 'Fibonacci',
-    7: 'Exit'
-}
-
-def printMenu():
-    for key in menulist.keys():
-        print(key, '--', menulist[key] )
-    menu()
-
+# def menu
+# using main_menu list:
+# 1. main menu and submenu reference are created [Prompts, Actions]
+# 2. menu_list is sent as parameter to menuy.menu function that has logic for menu control
 def menu():
-    while True:
-        try:
-            option = int(input('Choose one:  '))
-            if option == 1:
-                a = input("Enter a number")
-                b = input("Enter another number")
-                print(" ")
-                swap1_helper(a, b)
-            if option == 2:
-                height = int(input('Enter a height:  '))
-                for i in range(height):
-                    print((' ' * (height - i)) + ('*' * ((2 * i) + 1)))
-                print((' ' * height) + '**')
-                print((' ' * height) + '**')
-            elif option == 3:
-                test_matrices()
-            elif option == 4:
-                tester()
-            elif option == 5:
-                factorialTester()
-            elif option == 6:
-                fibonacci_tester()
-            elif option == 7:
-                print('Exiting')
-                exit()
-            else:
-                print('Invalid input. Enter a number between 1 and 3.')
-        except ValueError:
-            print('Invalid input. Please enter an integer input.')
+    title = "Function Menu" + banner
+    menu_list = main_menu.copy()
+    menu_list.append(["data", submenu_data])
+    menu_list.append(["math", submenu_math])
+    menu_list.append(["adventure", submenu_adventure])
+    buildMenu(title, menu_list)
 
-if __name__=='__main__':
-    printMenu()
+def submenu_data():
+    title = "Function Submenu" + banner
+    buildMenu(title, sub_menu_data)
+
+def submenu_math():
+    title = "Function Submenu" + banner
+    buildMenu(title, sub_menu_math)
+
+def submenu_adventure():
+    title = "Function Submenu" + banner
+    buildMenu(title, sub_menu_adventure)
+
+def buildMenu(banner, options):
+    # header for menu
+    print(banner)
+    # build a dictionary from options
+    prompts = {0: ["Exit", None]}
+    for op in options:
+        index = len(prompts)
+        prompts[index] = op
+
+    # print menu or dictionary
+    for key, value in prompts.items():
+        print(key, '->', value[0])
+
+    # get user choice
+    choice = input("Type your choice> ")
+
+    # validate choice and run
+    # execute selection
+    # convert to number
+    try:
+        choice = int(choice)
+        if choice == 0:
+            # stop
+            return
+        try:
+            # try as function
+            action = prompts.get(choice)[1]
+            action()
+        except TypeError:
+            try:  # try as playground style
+                exec(open(action).read())
+            except FileNotFoundError:
+                print(f"File not found!: {action}")
+            # end function try
+        # end prompts try
+    except ValueError:
+        # not a number error
+        print(f"Not a number: {choice}")
+    except UnboundLocalError:
+        # traps all other errors
+        print(f"Invalid choice: {choice}")
+    # end validation try
+
+    buildMenu(banner, options)  # recursion, start menu over again
+
+
+if __name__ == "__main__":
+    menu()
